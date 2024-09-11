0fd754eb73849c658fde44bb2935a32972e89c45
RamPrakashR12 <46301127+RamPrakashR12@users.noreply.github.com>
2024-09-11 23:28:42 +0530

    Add files via upload

---
+++ VersionCheck.java
@@ -0,0 +1,47 @@
+package PracticeSept01_01;
+
+import java.util.Scanner;
+
+public class VersionCheck {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Provide version 1 : ");
+
+		String version1 = input.nextLine();
+
+		System.out.println("Provide version 2 : ");
+
+		String version2 = input.nextLine();
+
+		String[] version01 = version1.split("\\.");
+		String[] version02 = version2.split("\\.");
+
+		int version1_lastvalue = 0, version2_lastvalue = 0, length = 0;
+
+		if (version02.length > version01.length) {
+			System.out.println("It is an Upgrade");
+		} else if (version02.length < version01.length) {
+			System.out.println("It is a Downgrade");
+		} else {
+			length = version01.length - 1;
+			for (int i = 0; i <= length; i++) {
+				version1_lastvalue = Integer.valueOf(version01[i]);
+				version2_lastvalue = Integer.valueOf(version02[i]);
+
+				if (version2_lastvalue > version1_lastvalue) {
+					System.out.println("It is an Upgrade");
+					break;
+				} else if (version2_lastvalue < version1_lastvalue) {
+					System.out.println("It is an Downgrade");
+					break;
+				}
+				if (i == length) {
+					System.out.println("No Change");
+				}
+			}
+		}
+
+		input.close();
+	}
+}
